79066b02cf378c7d4966b79b34d78be3aa6bc9d1
Patrick <Patrick@DESKTOP-IJ7Q23H>
2020-06-01 19:32:15 +0200

    Insertionsort

---
+++ Sortingalgorithms/Insertionsort/Insertionsort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insertionsort
+{
+    class Insertionsort
+    {
+        public static void Sort(int[] arr)
+        {
+            for(int i = 1; i< arr.Length; i++)
+            {
+                int valueToSort = arr[i];
+                int j = i;
+                while (j > 0 && arr[j-1] > valueToSort)
+                {
+                    arr[j] = arr[j - 1];
+                    j -= 1;
+                }
+                arr[j] = valueToSort;
+            }
+        }
+    }
+}
---
+++ Sortingalgorithms/Insertionsort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Datastructures.Datastructures;
+
+namespace Insertionsort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 10;
+            int range = 100;
+            int[] arrToSort = MyLists.generateList(0, range, length).ToArray();
+            Console.Write("Unsorted List: ");
+             foreach(int i in arrToSort)
+            {
+                Console.Write("{0} ", i);
+            }
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Insertionsort.Sort(arrToSort);
+            watch.Stop();
+            Console.Write("\nSorted List: ");
+            foreach (int i in arrToSort)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Insertionsort Execution Time: {watch.ElapsedMilliseconds} ms");
+            Console.ReadKey();
+        }
+    }
+}
