79a7bcbabc7d04f88f9b490f5e6ca7445bd8a7ed
Patrick <Patrick@DESKTOP-IJ7Q23H>
2020-06-26 19:36:35 +0200

    Quicksort

---
+++ Sortingalgorithms/Quicksort/Quicksort.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quicksort
+{
+    class Quicksort
+    {
+        public static List<int> runSynchron(List<int> listToSort, Boolean showProgress = false)
+        {
+            quicksort(listToSort, 0, listToSort.Count - 1);
+            return listToSort;
+        }
+
+        private static void quicksort(List<int> listToSort, int left, int right)
+        {
+            if (left < right)
+            {
+                int splitter = split(listToSort, left, right);
+                quicksort(listToSort, left, splitter - 1);
+                quicksort(listToSort, splitter + 1, right);
+            }
+        }
+
+        private static int split(List<int> listToSort, int left, int right)
+        {
+            int i = left;
+            int j = right;
+            int pivot = listToSort[right];
+            while (i < j)
+            {
+                while (i < right && listToSort[i] < pivot)
+                {
+                    i += 1;
+                }
+
+                while (j > left && listToSort[j] >= pivot)
+                {
+                    j -= 1;
+                }
+
+                if (i < j)
+                {
+                    listToSort[i] ^= listToSort[j];
+                    listToSort[j] ^= listToSort[i];
+                    listToSort[i] ^= listToSort[j];
+                }
+
+                if (listToSort[i] > pivot)
+                {
+                    listToSort[i] ^= listToSort[right];
+                    listToSort[right] ^= listToSort[i];
+                    listToSort[i] ^= listToSort[right];
+                }
+            }
+            return i;
+        }
+    }
+}
