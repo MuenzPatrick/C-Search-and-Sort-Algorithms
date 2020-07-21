6459cb07c531eff0b38ed2d6e8243188bda25ae4
Patrick <Patrick@DESKTOP-IJ7Q23H>
2020-07-21 12:37:00 +0200

    Selectionsort

---
+++ Sortingalgorithms/Selectionsort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Datastructures.Datastructures;
+
+namespace Selectionsort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listToSort = MyLists.generateList(0, 100, 10);
+            Console.Write("Unsorted List: ");
+            listToSort.ForEach(i => Console.Write("{0} ", i));
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            List<int> sortedList = Selectionsort.runSynchron(listToSort);
+            watch.Stop();
+            Console.Write("\nSorted List: ");
+            sortedList.ForEach(i => Console.Write("{0} ", i));
+            Console.WriteLine();
+            Console.WriteLine($"Selectionsort Execution Time: {watch.ElapsedMilliseconds} ms");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sortingalgorithms/Selectionsort/Selectionsort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selectionsort
+{
+    class Selectionsort
+    {
+        public static List<int> runSynchron(List<int> listToSort, Boolean showProgress = false)
+        {
+            int maxIdx = listToSort.Count - 1;
+            int currIdx = 0;
+            do
+            {
+                int minPos = currIdx;
+                for (int idx = currIdx + 1; idx <= maxIdx; idx++)
+                {
+                    if (listToSort[idx] < listToSort[minPos])
+                    {
+                        minPos = idx;
+                    }
+                }
+                listToSort[minPos] ^= listToSort[currIdx];
+                listToSort[currIdx] ^= listToSort[minPos];
+                listToSort[minPos] ^= listToSort[currIdx];
+                currIdx++;
+            } while (currIdx < maxIdx);
+            return listToSort;
+        }
+    }
+}
