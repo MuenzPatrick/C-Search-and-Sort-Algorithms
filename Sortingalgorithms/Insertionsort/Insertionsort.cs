3788fbe9bb932548d7a622d00ef447d9f496ba60
Patrick <Patrick@DESKTOP-IJ7Q23H>
2020-07-21 17:44:27 +0200

    shellsort, insertionsort update

--- Sortingalgorithms/Insertionsort/Insertionsort.cs
+++ Sortingalgorithms/Insertionsort/Insertionsort.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Insertionsort
 {
-    class Insertionsort
+    public class Insertionsort
     {
         public static void Sort(int[] arr)
         {
@@ -18,6 +18,20 @@ namespace Insertionsort
                 {
                     arr[j] = arr[j - 1];
                     j -= 1;
+                }
+                arr[j] = valueToSort;
+            }
+        }
+        public static void Sort_Matrix(int[] arr, int columns)
+        {
+            for (int i = columns; i < arr.Length; i++)
+            {
+                int valueToSort = arr[i];
+                int j = i;
+                while (j > 0 && arr[j - columns] > valueToSort)
+                {
+                    arr[j] = arr[j - columns];
+                    j -=  columns;
                 }
                 arr[j] = valueToSort;
             }
---
+++ Sortingalgorithms/Shellsort/Selectionsort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Insertionsort;
+namespace Shellsort
+{
+    class Shellsort
+    {
+        public static  List<int> runSynchron(List<int> listToSort)
+        {
+            int k, h;
+            int[] arrayToSort = listToSort.ToArray();
+            int[] spalten = {
+                2147483647, 1131376761, 410151271, 157840433,
+                58548857, 21521774, 8810089, 3501671, 1355339, 543749, 213331,
+                84801, 27901, 11969, 4711, 1968, 815, 271, 111, 41, 13, 4, 1
+            };
+            for (k = 0; k < spalten.Length; k++)
+            {
+                h = spalten[k];
+                Insertionsort.Insertionsort.Sort_Matrix(arrayToSort, h);
+            }
+            return arrayToSort.ToList();
+        }
+    }
+}
