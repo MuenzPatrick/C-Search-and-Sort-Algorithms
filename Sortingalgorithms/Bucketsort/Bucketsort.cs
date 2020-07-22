3009dd76e747e224bf08530c580085bc0bbc0425
Patrick <Patrick@DESKTOP-IJ7Q23H>
2020-07-22 11:51:31 +0200

    Bucketsort

---
+++ Sortingalgorithms/Bucketsort/Bucketsort.cs
@@ -0,0 +1,39 @@
+﻿using Insertionsort;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bucketsort
+{
+    class Bucketsort
+    {
+        public static List<int> runSynchron(int[] listToSort)
+        {
+            List<int> sortedList = new List<int>();
+            int max = Convert.ToInt32(Math.Floor(Math.Log10(listToSort.Max())));
+            max = Convert.ToInt32(Math.Pow(10, max));
+            int numOfBuckets = 10;
+            List<int>[] buckets = new List<int>[numOfBuckets];
+            for (int i = 0; i < numOfBuckets; i++)
+            {
+                buckets[i] = new List<int>();
+            }
+            //Iterate through the passed array and add each integer to the appropriate bucket
+            for (int i = 0; i < listToSort.Length; i++)
+            {
+                int bucket = (listToSort[i] / (1*max));
+                buckets[bucket].Add(listToSort[i]);
+            }
+            //Sort each bucket and add it to the result List
+            for (int i = 0; i < numOfBuckets; i++)
+            {
+                int[] a = buckets[i].ToArray(); 
+                Insertionsort.Insertionsort.Sort(a);
+                sortedList.AddRange(a);
+            }
+            return sortedList;
+        }
+    }
+}
---
+++ Sortingalgorithms/Bucketsort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Datastructures.Datastructures;
+
+namespace Bucketsort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 10;
+            int range = 100;
+            int[] arrToSort = MyLists.generateList(0, range, length).ToArray();
+            Console.Write("Unsorted List: ");
+            foreach (int i in arrToSort)
+            {
+                Console.Write("{0} ", i);
+            }
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var sortedList = Bucketsort.runSynchron(arrToSort);
+            watch.Stop();
+            Console.Write("\nSorted List: ");
+            foreach (int i in sortedList)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Bucketsort Execution Time: {watch.ElapsedMilliseconds} ms");
+            Console.ReadKey();
+        }
+    }
+}
