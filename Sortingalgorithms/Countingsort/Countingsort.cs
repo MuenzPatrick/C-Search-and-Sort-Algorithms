ea18f9f0cc1a7c07c34955e2ffc7aa19b99b679b
Patrick <Patrick@DESKTOP-IJ7Q23H>
2020-07-23 09:40:54 +0200

    Coutingsort

---
+++ Sortingalgorithms/Countingsort/Countingsort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Countingsort
+{
+    class Countingsort
+    {
+        public static void runSynchron(List<int> listToSort)
+        {
+            int maxNumber = listToSort.Max();
+            int[] tempList = new int[maxNumber + 1];
+            for(int i = 0; i < listToSort.Count; i++)
+            {
+                tempList[listToSort[i]]++;
+            }
+            int finalposition = 0;
+            for(int i = 0; i <= maxNumber; i++)
+            {
+                for( int j = 0; j < tempList[i]; j++)
+                {
+                    listToSort[finalposition] = i;
+                    finalposition++;
+                }
+            }
+        }
+    }
+}
